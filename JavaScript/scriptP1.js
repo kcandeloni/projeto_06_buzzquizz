ea6aa51dccd3da40dd3a5540382c170e2e0d0a76
kcandeloni <kcandeloni@gmail.com>
2022-06-29 21:13:25 -0300

    ajuste tela tela 1

--- JavaScript/scriptP1.js
+++ JavaScript/scriptP1.js
@@ -1,3 +1,5 @@
+const linkQuizzes = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+
 function openTela (fecha , abre){
     document.querySelector(`.${fecha}`).classList.toggle('escondido');
     document.querySelector(`.${abre}`).classList.toggle('escondido');
